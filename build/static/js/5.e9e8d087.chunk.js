9ab1a21ee234c74b6e22ab30aeb5a91959346194
SuperSMF <Supersmf@mail.ru>
2018-12-24 01:42:49 +0300

    remove build package

--- build/static/js/5.e9e8d087.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{84:function(e,t,n){"use strict";n.r(t);var a=n(0),c=n.n(a);t.default=function(){return c.a.createElement("div",{className:"d-flex justify-content-center mt-5 pt-5"},c.a.createElement("h1",{className:" bg-light col-4 text-center font-weight-bold p-5"},"This page isn't exist"))}}}]);
-//# sourceMappingURL=5.e9e8d087.chunk.js.map
